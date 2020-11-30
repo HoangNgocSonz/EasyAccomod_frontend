dfc80974dede571b0430886dc8ea4e99b3d3a88b
hongthamuet <56645746+hongthamuet@users.noreply.github.com>
2020-12-01 00:51:13 +0700

    form login 1.0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Navbar from "./component/Navbar";
 import Register from "./component/register";
+import Login from "./component/login";
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Login></Login>
+      <Register></Register>
     </div>
   );
 }
