c68a1de9b26bcd50463eb95daea2dd0b93ad1992
HoangNgocSonz <hoangson20bg@gmail.com>
2020-12-20 13:00:18 +0700

    pull

--- src/component/home/roomList/RoomList.js
+++ src/component/home/roomList/RoomList.js
@@ -52,46 +52,46 @@ export default class RoomList extends Component {
     ));
     return (
       <div>
-        <Container>
-          <Row>
-            <Col xs={12} sm={12} md={12} lg={9} xl={9}>
-              {contentArr}
-            </Col>
-            <Col xs={0} sm={0} md={0} lg={3} xl={3}>
-              <div className="filter">
-                <div className="filterCost">
-                  <h4 className="filterCostTitle">Lọc theo khoảng giá</h4>
-                  <hr width="80%"></hr>
-                  <p className="selectCost">0 - 1 triệu</p>
-                  <p className="selectCost">1 - 2 triệu</p>
-                  <p className="selectCost">2 - 3 triệu</p>
-                  <p className="selectCost">3 - 5 triệu</p>
-                  <p className="selectCost">5 - 7 triệu</p>
-                  <p className="selectCost">7 - 10 triệu</p>
-                  <p className="selectCost">10 - 20 triệu</p>
-                  <p className="selectCost">20 - 50 triệu</p>
-                  <p className="selectCost">50 - 100 triệu</p>
-                  <p className="selectCost">trên 100 triệu</p>
-                </div>
-                <div className="filterAceage">
-                  <h4 className="filterCostTitle">Lọc theo diện tích</h4>
-                  <hr width="80%"></hr>
-                  <p className="selectCost">0 - 15 m2</p>
-                  <p className="selectCost">15 - 20 m2 triệu</p>
-                  <p className="selectCost">20 - 25 m2 triệu</p>
-                  <p className="selectCost">25 - 30 m2 triệu</p>
-                  <p className="selectCost">30 - 35 m2 triệu</p>
-                  <p className="selectCost">35 - 40 m2 triệu</p>
-                  <p className="selectCost">40 - 50 m2 triệu</p>
-                  <p className="selectCost">50 - 60 m2 triệu</p>
-                  <p className="selectCost">60 - 80 m2 triệu</p>
-                  <p className="selectCost">80 - 100 m2 triệu</p>
-                  <p className="selectCost">trên 100 m2</p>
-                </div>
+        <Row>
+          <Col xl={1}></Col>
+          <Col xs={12} sm={12} md={12} lg={9} xl={8}>
+            {contentArr}
+          </Col>
+          <Col xs={0} sm={0} md={0} lg={3} xl={2}>
+            <div className="filter">
+              <div className="filterCost">
+                <h4 className="filterCostTitle">Lọc theo khoảng giá</h4>
+                <hr width="80%"></hr>
+                <p className="selectCost">0 - 1 triệu</p>
+                <p className="selectCost">1 - 2 triệu</p>
+                <p className="selectCost">2 - 3 triệu</p>
+                <p className="selectCost">3 - 5 triệu</p>
+                <p className="selectCost">5 - 7 triệu</p>
+                <p className="selectCost">7 - 10 triệu</p>
+                <p className="selectCost">10 - 20 triệu</p>
+                <p className="selectCost">20 - 50 triệu</p>
+                <p className="selectCost">50 - 100 triệu</p>
+                <p className="selectCost">trên 100 triệu</p>
               </div>
-            </Col>
-          </Row>
-        </Container>
+              <div className="filterAceage">
+                <h4 className="filterCostTitle">Lọc theo diện tích</h4>
+                <hr width="80%"></hr>
+                <p className="selectCost">0 - 15 m2</p>
+                <p className="selectCost">15 - 20 m2 triệu</p>
+                <p className="selectCost">20 - 25 m2 triệu</p>
+                <p className="selectCost">25 - 30 m2 triệu</p>
+                <p className="selectCost">30 - 35 m2 triệu</p>
+                <p className="selectCost">35 - 40 m2 triệu</p>
+                <p className="selectCost">40 - 50 m2 triệu</p>
+                <p className="selectCost">50 - 60 m2 triệu</p>
+                <p className="selectCost">60 - 80 m2 triệu</p>
+                <p className="selectCost">80 - 100 m2 triệu</p>
+                <p className="selectCost">trên 100 m2</p>
+              </div>
+            </div>
+          </Col>
+          <Col xl={1}></Col>
+        </Row>
       </div>
     );
   }
