fc5f4751c389ea02d98034889da685fd02fbfa99
MaiLinh123 <48786774+MaiLinh123@users.noreply.github.com>
2020-12-22 00:52:38 +0700

    new_news ver 1.2

--- src/component/newHome/new_news/new_news.js
+++ src/component/newHome/new_news/new_news.js
@@ -1,7 +1,15 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+import { Multiselect } from 'multiselect-react-dropdown';
 import './new_news.css'
 
-class NewsHome extends Component{
+class NewsHome extends Component{    
+    constructor(props){
+        super(props);
+        this.state = {
+            options: [{name: 'Wifi miễn phí', id: 1},{name: 'Có gác lửng', id: 2},{name: 'Tủ + giường', id: 3},{name: 'Không chung chủ', id: 4},
+            {name: 'Chung chủ', id: 5}, {name: 'Giờ giấc tự do', id: 6}, {name: 'Vệ sinh riêng', id: 7}, {name: 'Vệ sinh chung', id: 8}],
+        }
+    }
     render(){
         return(
             <div className="container-fluid newshome">
@@ -19,57 +27,48 @@ class NewsHome extends Component{
                                 <div className = "panel-body">               
                                     <input type="hidden" name="_token"/>
                                     <div className="form-group">
-                                        <label for="usr">Tiêu đề bài đăng:</label>
-                                        <input type="text" className="form-control" name="txttitle"/>
+                                        <label for="head">Tiêu đề bài đăng:</label>
+                                        <input type="text" className="form-control" name="txttitle" id = "head"/>
                                     </div>
                                     <div className="row">
                                         <div className="col-md-3 col-sm-3 col-xs-12">
                                             <div className="form-group">
-                                                <label for="usr">Tỉnh/ Thành phố:</label>
-                                                <select className="form-control nice-select wide select_item" name="Haha">
+                                                <label for="city">Tỉnh/ Thành phố:</label>
+                                                <select className="form-control nice-select wide select_item" name="Haha" id = "city">
                                                 <option value='0' name="0">-- Chọn Tỉnh/Thành Phố --</option>
                                                 </select>
                                             </div>
                                         </div>
                                         <div className="col-md-3 col-sm-3 col-xs-12">
                                             <div className="form-group">
-                                                <label for="usr">Quận/ Huyện:</label>
-                                                <select className="form-control nice-select wide select_item">
+                                                <label for="add">Quận/ Huyện:</label>
+                                                <select className="form-control nice-select wide select_item" id = "add">
                                                     <option value='0'>-- Chọn Quận/Huyện --</option>
                                                 </select>
                                             </div>
                                         </div>
                                         <div className="col-md-3 col-sm-3 col-xs-12">
                                             <div className="form-group">
-                                                <label for="usr">Tên đường</label>
-                                                <select className="form-control nice-select wide select_item">
+                                                <label for="name">Tên đường</label>
+                                                <select className="form-control nice-select wide select_item" id = "name">
                                                     <option value='0'>-- Chọn Tên Đường --</option>
                                                 </select>
                                             </div>
                                         </div>
                                         <div className="col-md-3 col-sm-3 col-xs-12">
                                             <div className="form-group">
-                                                <label for="usr">Danh mục:</label>
-                                                <select className="form-control nice-select wide select_item">
+                                                <label for="items">Danh mục:</label>
+                                                <select className="form-control nice-select wide select_item" id = "items">
                                                     <option value="1">Thuê phòng trọ</option>
                                                     <option value="2">Thuê nhà nguyên căn</option>
                                                     <option value="3">Thuê căn hộ</option>
                                                 </select>
                                             </div>
                                         </div>
-                                        {/*<div className="col-md-4 col-sm-4 col-xs-12">
-                                            <div className="col-md-10 col-sm-10 col-xs-12">
-                                            <select className="form-control nice-select wide select_item">
-                                                    <option value="1">Thuê phòng trọ</option>
-                                                    <option value="2">Thuê nhà nguyên căn</option>
-                                                    <option value="3">Thuê căn hộ</option>
-                                                </select>
-                                            </div>
-                                        </div>*/}
                                     </div>
                                     <div className="form-group">
-                                        <label>Địa chỉ phòng trọ:</label> Bạn có thể nhập hoặc chọn ví trí trên bản đồ 
-                                        <input type="text" id="location-text-box" name="txtaddress" className="form-control" value="" />
+                                        <label for ="address">Địa chỉ phòng trọ:</label> Bạn có thể nhập hoặc chọn ví trí trên bản đồ 
+                                        <input type="text" id="location-text-box" name="txtaddress" className="form-control" value="" id = "address" />
                                         <input type="hidden" id="txtaddress" name="txtaddress" value=""  className="form-control"  />
                                         <input type="hidden" id="txtlat" value="" name="txtlat"  className="form-control"  />
                                         <input type="hidden" id="txtlng"  value="" name="txtlng" className="form-control" />
@@ -101,14 +100,14 @@ class NewsHome extends Component{
                                     <div className="row">
                                         <div className="col-md-6">
                                             <div className="form-group">
-                                                <label for="usr">Giá phòng( vnđ ):</label>
-                                                <input type="number" name="txtprice" className="form-control" placeholder="750000"/>
+                                                <label for="cost">Giá phòng( vnđ ):</label>
+                                                <input type="number" name="txtprice" className="form-control" placeholder="750000" id = "cost"/>
                                             </div>
                                         </div>
                                         <div className="col-md-6">
                                             <div className="form-group">
-                                                <label for="usr">Diện tích( m<sup>2</sup> ):</label>
-                                                <input type="number" name="txtarea" className="form-control" placeholder="16"/>
+                                                <label for="s">Diện tích( m<sup>2</sup> ):</label>
+                                                <input type="number" name="txtarea" className="form-control" placeholder="16" id = "s"/>
                                             </div>
                                         </div>
                                     </div>
@@ -116,23 +115,28 @@ class NewsHome extends Component{
                                         <div className="col-md-4">
                                             <div className="form-group">
                                             <label for="usr">SĐT Liên hệ:</label>
-                                            <input type="text" name="txtphone" className="form-control" placeholder="0915111234"></input>
+                                            <input type="text" name="txtphone" className="form-control" placeholder="0915111234" id = "usr"></input>
                                             </div>
                                         </div>
                                     </div> 
-                                    <div className="form-group">
-                                        <label>Các tiện ích có trong phòng trọ:</label>
-                                        <select id="select-state" name="tienich[]" multiple className="demo-default selectized" placeholder="Chọn các tiện ích phòng trọ">
-                                            <option value="Wifi miễn phí">Wifi miễn phí</option>
-                                            <option value="Có gác lửng">Có gác lửng</option>
-                                            <option value="Tủ + giường">Tủ + giường</option>
-                                            <option value="Không chung chủ">Không chung chủ</option>
-                                            <option value="Chung chủ" >Chung chủ</option>
-                                            <option value="Giờ giấc tự do">Giờ giấc tự do</option>
-                                            <option value="Vệ sinh riêng">Vệ sinh riêng</option>
-                                            <option value="Vệ sinh chung">Vệ sinh chung</option>
-                                        </select>
-                                    </div>
+                                    <form>
+                                        <div className="form-group">
+                                            <label for = "dropdown-test">Các tiện ích có trong phòng trọ:</label>
+                                            <Multiselect
+                                                options={this.state.options}
+                                                displayValue="name"
+                                                showCheckbox={true}
+                                                closeIcon = "cancel"
+                                                placeholder = "Lựa chọn tiện ích"
+                                            />
+                                        </div>
+                                    </form>
+                                    <form>
+                                        <div class="form-group">
+                                            <label for="exampleFormControlFile1">Thêm ảnh minh họa</label>
+                                            <input type="file" class="form-control-file" id="exampleFormControlFile1" multiple/>
+                                        </div>
+                                    </form>
                                     <button class="btn_PostNews">Đăng Tin</button>
                                 </div>
                             </div>
