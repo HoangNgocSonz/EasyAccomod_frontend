5dac642cd947c34d7c0fd8f40c414d02d926968b
MaiLinh123 <48786774+MaiLinh123@users.noreply.github.com>
2020-12-25 01:51:07 +0700

    admin ver 1.3

--- src/component/admin/manage_post/manage_post.js
+++ src/component/admin/manage_post/manage_post.js
@@ -1,40 +1,39 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, {Component} from "react";
 
-
-import './post_management.css'
-class Postmanagement extends Component {
+import './manage_post.css'
+class ManagePost extends Component{
     render() {
         return (
-            <div class = "post_container">
+            <div class = "post_container" data-wow-delay="0.2s">
                 <div className = "tieudepage_mg">
                     <h2 className="tieudepage_mg-h2">Quản lý tin Phòng Trọ</h2>
                     <p>Thông tin càng chính xác giúp cho người thuê một cách tốt nhất</p>
                 </div>
             
-                <div data-wow-delay="0.1s"> 
-                
-                        <table className="table table-striped" data-wow-delay="0.2s" id = "customers">
+                <div data-wow-delay="0.1s" class = "main_table"> 
+                    <div id = "main_table">
+                    <br/>
+                        <table className="table-striped" data-wow-delay="0.2s" id = "customers">
                         <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Ảnh đại diện</th>
-                            <th>Tiêu đề</th>
-                            <th>Giá</th>
-                            <th>Ngày bắt đầu</th>
-                            <th>Ngày kết thúc</th>
-                            <th>Trạng thái</th>
-                        </tr>
+                            <tr>
+                                <th>ID</th>
+                                <th>Tiêu đề</th>
+                                <th>Thời gian tạo</th>
+                                <th>Người tạo</th>
+                                <th>Trạng thái</th>
+                                <th>Ngày bắt đầu</th>
+                                <th>Ngày kết thúc</th>
+                            </tr>
                         </thead>
                         <tbody>
                             <tr>
                                 <td>1</td>
-                                <td></td>
                                 <td>Cho thuê phòng trọ</td>
-                                <td>2 000 000 VNĐ</td>
-                                <td>Hoạt động</td>
-                                <td>Không</td>
-                                <td>Không</td>
+                                <td>25/12/2020</td>
+                                <td>mailinh1352000@gmail.com</td>
+                                <td>Chưa duyệt</td>
+                                <td>26/12/2020</td>
+                                <td>26/02/2020</td>
                             </tr>
                             <tr>
                                 <td>2</td>
@@ -56,10 +55,10 @@ class Postmanagement extends Component {
                             </tr>
                         </tbody>
                     </table>
+                    </div>
                 </div>
             </div>
         );
     }
 }
-
-export default Postmanagement;
+export default ManagePost;
--- src/component/admin/manage_uses/manage_uses.js
+++ src/component/admin/manage_uses/manage_uses.js
@@ -7,53 +7,56 @@ class ManageUse extends Component{
         return(
             <div class = "post_container">
                 <div className = "tieudepage_mg">
-                    <h2 className="tieudepage_mg-h2">Quản lý người dùng</h2>
+                    <h2 className="tieudepage_mg-h2">Quản lý chủ trọ</h2>
+                    <p>Thông tin càng chính xác giúp cho người thuê một cách tốt nhất</p>
                 </div>
             
                 <div data-wow-delay="0.1s"> 
-                
-                        <table className="table table-striped" data-wow-delay="0.2s" id = "customers">
-                        <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Ảnh đại diện</th>
-                            <th>Email</th>
-                            <th>Quyền</th>
-                            <th>Trạng thái</th>
-                            <th>Chỉnh sửa</th>
-                            <th>Xóa người dùng</th>
-                        </tr>
-                        </thead>
-                        <tbody>
+                    <div id = "main_table">
+                    <br/>
+                        <table className="table-striped" data-wow-delay="0.2s" id = "customers">
+                            <thead>
                             <tr>
-                                <td>1</td>
-                                <td></td>
-                                <td>mailinh1352000@gmail.com</td>
-                                <td>Admin</td>
-                                <td>Hoạt động</td>
-                                <td>Không</td>
-                                <td>Không</td>
+                                <th>ID</th>
+                                <th>Ảnh đại diện</th>
+                                <th>Email</th>
+                                <th>Quyền</th>
+                                <th>Trạng thái</th>
+                                <th>Chỉnh sửa</th>
+                                <th>Xóa người dùng</th>
                             </tr>
-                            <tr>
-                                <td>2</td>
-                                <td></td>
-                                <td>mailinh1352000@gmail.com</td>
-                                <td>Admin</td>
-                                <td>Hoạt động</td>
-                                <td>Không</td>
-                                <td>Không</td>
-                            </tr>
-                            <tr>
-                                <td>3</td>
-                                <td></td>
-                                <td>mailinh1352000@gmail.com</td>
-                                <td>Admin</td>
-                                <td>Hoạt động</td>
-                                <td>Không</td>
-                                <td>Không</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td>mailinh1352000@gmail.com</td>
+                                    <td>Admin</td>
+                                    <td>Hoạt động</td>
+                                    <td>Không</td>
+                                    <td>Không</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td></td>
+                                    <td>mailinh1352000@gmail.com</td>
+                                    <td>Admin</td>
+                                    <td>Hoạt động</td>
+                                    <td>Không</td>
+                                    <td>Không</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td></td>
+                                    <td>mailinh1352000@gmail.com</td>
+                                    <td>Admin</td>
+                                    <td>Hoạt động</td>
+                                    <td>Không</td>
+                                    <td>Không</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         );
